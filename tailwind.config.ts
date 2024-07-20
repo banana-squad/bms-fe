2fd23ce5f73aa64c1be6ae70ad1b5321cd8fefcc
ukyi <ukyi.js@gmail.com>
2024-07-20 15:28:16 +0900

    feat: nes-dark color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,11 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nes-dark': '#212529',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
