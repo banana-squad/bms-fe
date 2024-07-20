48c2be2f3985d83cac05692b600bca4f51ab4bb1
ukyi <ukyi.js@gmail.com>
2024-07-20 14:23:26 +0900

    feat: nes.css 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ReactQueryProvider } from '@/shared/providers/reactQuery.provider';
 import { ReactRouterProvider } from '@/shared/providers/reactRouter.provider';
 import { ModalProvider } from '@/shared/ui/modal/modal.provider';
 import ReactDOM from 'react-dom/client';
+import 'nes.css/css/nes.min.css';
 import '@/shared/styles/index.css';
 
 const root = document.getElementById('root') as HTMLElement;
